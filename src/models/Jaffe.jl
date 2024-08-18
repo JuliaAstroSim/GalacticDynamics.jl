58ae3e1fa9943290e535d67eb0d7972c8ffcf210
islent <leoislent@gmail.com>
2024-08-18 21:03:27 +0800

    add Jaffe density

--- src/models/Jaffe.jl
+++ src/models/Jaffe.jl
@@ -11,3 +11,10 @@ $(TYPEDSIGNATURES)
 function potential(r, a, ρ₀, G, ::Jaffe)
     return -4π*G*ρ₀*a^2 * ln(1+a/r)
 end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function density(r, a, ρ₀, ::Jaffe)
+    return ρ₀ / (r/a)^2 / (r/a + 1)^2
+end

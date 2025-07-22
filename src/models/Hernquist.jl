0024a6f5b0213b0c888995140e0a9d516744c93b
islent <leoislent@gmail.com>
2025-07-22 12:54:23 +0800

    density of Hernquist model [skip ci]

--- src/models/Hernquist.jl
+++ src/models/Hernquist.jl
@@ -13,6 +13,13 @@ scale_radius(model::Hernquist) = model.r_s
 """
 $(TYPEDSIGNATURES)
 """
+function density(model::Hernquist, r)
+    return model.ρ₀ / (r/model.r_s) / (1+r/model.r_s)^3
+end
+
+"""
+$(TYPEDSIGNATURES)
+"""
 function mass_interior(model::Hernquist, r)
     return 4π*model.ρ₀*model.r_s^3 * 0.5*(r/model.r_s)^2 / (1+r/model.r_s)^2
 end
